f6f6839cefc186f5417a1b151b9cb9607fc44e90
Tony <1462668901@qq.com>
2021-04-29 03:15:18 +0800

    Update nav.js

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,15 +1,11 @@
 module.exports = [
   {text: '首页', link: '/'},
   {
-    text: '前端',
-    link: '/web/',  //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    text: '树莓派',
+    link: '/rpi/',  //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
       {
-        text: '学习笔记', 
-        items:
-        [
-          {text: 'Vue笔记', link: '/vue/'}
-        ]}
+          {text: '树莓派操作系统大全', link: '/rpi-systems/'}
     ]
   },
   {
